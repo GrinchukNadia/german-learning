8c97e10962957a45c3aece2a000ba50d48fdffd2
Надежда <grennad991@gmail.com>
2025-02-26 19:23:22 +0100

    Update project

--- src/Card.js
+++ src/Card.js
@@ -4,28 +4,33 @@ function Card(props) {
   const animation = { animationName: props.animation };
   return (
     <div className='card'>
-      <div className='article article-der' data-article="der">
+      <div className='article article-der' data-article='der'>
         <div className='article-arrow article-arrow-left'></div>
         <div>der</div>
       </div>
-      <div className='article article-die' data-article="die">
+      <div className='article article-die' data-article='die'>
         <div className='article-arrow article-arrow-right'></div>
         <div>die</div>
       </div>
-      <div className='article article-das' data-article="das">
+      <div className='article article-das' data-article='das'>
         <div className='article-arrow article-arrow-top'></div>
         <div>das</div>
       </div>
-      <div className='article article-next' data-article="next">
+      <div className='article article-next' data-article='next'>
         <div>nächste</div>
         <div className='article-arrow article-arrow-bottom'></div>
       </div>
-      <div style={animation} className={`card-inner ${props.style}`}>
-        <div className='card-inner_img'>
+      <div
+        style={animation}
+        className={`card-inner ${props.style}`}
+      >
+        {/* <div className='card-inner_img'>
           <img alt='oil' src={props.link}></img>
-        </div>
+        </div> */}
         <div className='card-inner_text'>
-          <div>{props.article} {props.word}</div>
+          <div>
+            {props.article} {props.word}
+          </div>
           <div className='card-inner_translate'>{props.translation}</div>
         </div>
       </div>
